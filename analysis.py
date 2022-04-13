7d9cbd2495b0b3005596691fa17a27b0a1d55487
Sean Elliott <G00411288@gmit.ie>
2022-04-13 21:02:14 +0100

    Finally got the columns organised - now to break down into groups

--- analysis.py
+++ analysis.py
@@ -15,9 +15,14 @@ filename = 'iris.data'
 # I open the file in read mode so that we can extrapolate the data from the data set. I ensure that the mode is set to 'rt' or 
 # read text mode; to ensure no overwriting of the data within the file occurs.
 with open (filename, "rt") as f: 
-    data = pd.read_csv(filename)
+    data = pd.read_csv(filename, header=None)
 
 # upon opening the file I then designate the columns of information so that they are easily readable. I found the colum headings in the original webiste description.
-    cols = ['Sepal Length', 'Sepal Width', 'Petal Length', 'Petal Width', 'Type']
+#data.rename(columns = {0:'Sepal Length',1:'Sepal Width',2:'Petal Length',3:'Petal Width',4:'Type'})
+#data.to_csv(filename, index=False)
+    data.columns = ['Sepal Length','Sepal Width','Petal Length','Petal Width','Type'] 
     print (data)
+
+# We then start by organising the data within the file to start making sense of the data. 
+
 
