0eafdf4013d7f5176835735a7cd6318a3f0732a9
Sean Elliott <G00411288@gmit.ie>
2022-04-21 09:55:16 +0100

    Added boxplot, jointplot and updated readme and started inital info edit

--- README.md
+++ README.md
@@ -6,7 +6,12 @@ According to 'https://archive.ics.uci.edu/ml/datasets/iris':
 
 "This is perhaps the best known database to be found in the pattern recognition literature. Fisher's paper is a classic in the field and is referenced frequently to this day. ... The data set contains 3 classes of 50 instances each, where each class refers to a type of iris plant. One class is linearly separable from the other 2; the latter are NOT linearly separable from each other." 
 
+According to 'https://rpubs.com/AjinkyaUC/Iris_DataSet': 
+
+"It is sometimes called Anderson’s Iris data set because Edgar Anderson collected the data to quantify the morphologic variation of Iris flowers of three related species.Two of the three species were collected in the Gaspé Peninsula “all from the same pasture, and picked on the same day and measured at the same time by the same person with the same apparatus”.
+
 The information that is included in the data set is as follows:
+
 1) Sepal length in cm 
 2) Sepal width in cm 
 3) Petal length in cm 
@@ -24,6 +29,20 @@ came between 5cm and 7cm in length, with the mean measurement coming in at 5.84c
 Petal Length and Width provide much starker sets of information. Iris Setosa's petal sizes, both in width and length were dwarfed by the other two species - with petal length of between 1cm and 2cm - with the average being 1.5cm and the average petal width being between 0.1cm and 0.5cm. According to the dataset provided the Iris Versicolor has longer petals starting at 3cm and finishing just over 5cm in length - with the average being 4.00cm. The petal width measurements were between 1.0cm and 1.75cm respectively in the Iris Versicolor, with an average measurement of 1.375cm.
 
 According to the dataset the Iris Virginica has much longer and wider petals than the other two species - the average petal length ranged between 5cm and 7cm with the average being 6cm, while the petal width went from 1.5cm to 2.5cm - with the average being 2cm in width. Compared to the setosa the Virginica also has longer sepal length from 5.5cm to 8cm in length. The Sepal width in the Virginica was also fairly average - spanning from 2.5cm to 3.75cm in width with the average reading being 3.0cm.
+
+**<p align="center"> Walkthrough of Program**
+
+I found this quite a challenging program to write. It combined many aspects fo the course which we had covered, but also required me to learn how to use seaborn - which I found to be an excellent tool for data visualisation. The program starts by reading in the dataset 'iris.data' and outputs a summary of the data to a single text file. I then go on to analyse the data in various ways using the 3 species of Iris Flower; Iris Setosa, Iris Virginica and Iris Versicolor.
+
+Using seaborn I started by creating a histogram plot of the three species of flower, comparing sepal length, sepal width, petal length and petal width across the three species. I then created a distplot which combines the above histogram with a trendline running through it. I found this to be more visually appealing than just the histogram plot on it's own.
+
+I then went on to create a scatterplot of the 3 species which would help me to compare the sepal length/sepal width and the petal length/petal width of the 3 species. I included a legend with each scatterplot and made sure to correctly label the axes to avoid any confusion. I also then allow the program to save the two scatterplots as .png files to the repository for viewing.
+
+I then created a pairplot - this is a combination all of the above data - combining the scatter plots and also distplots to show all of the information in one place. I found this to be particularly appealing to look at and found that it helped give me a much wider view of the information contained within the dataset.
+
+I decided to add a boxplot as another challenge to myself to represent the data in a different way than I would have been used to. The boxplot showed some interesting differences between the species - showing that the Iris Setosa has much smaller petals than the other two species. It also clearly shows that the iris Virginica is the larger of the three species with much larger petals and only slightly smaller sepal width.
+
+Finally I added a jointplot which I found in the seaborn documentation. I found this combination of the scatterplot in the centre with the trendlines on the exterior a very appealing way of comparing the data - and used the program to compare Petal Length and Sepal Length.
 
 **<p align="center"> References**
 
@@ -58,3 +77,12 @@ https://www.delftstack.com/howto/seaborn/legend-seaborn-plot/
 https://stackoverflow.com/questions/36018681/stop-seaborn-plotting-multiple-figures-on-top-of-one-another 
 
 https://seaborn.pydata.org/tutorial/function_overview.html
+
+https://www.codecademy.com/article/seaborn-design-ii 
+
+https://www.geeksforgeeks.org/python-pandas-dataframe-corr/ 
+
+https://seaborn.pydata.org/generated/seaborn.boxplot.html 
+
+https://seaborn.pydata.org/tutorial/function_overview.html
+
--- analysis.py
+++ analysis.py
@@ -21,7 +21,6 @@ columns = ['Sepal Length','Sepal Width','Petal Length','Petal Width','Type']
     
 with open (filename, "rt") as f: 
     iris = pd.read_csv(filename, header=None, names=columns)
-    print (iris)
     
 # I then ask the program to break the information down into more readable chunks - I do this by asking it to define the grouping (ie. the 3 different types of flower)
 # so that the user of the program has the data at hand from the offset.
@@ -29,11 +28,14 @@ with open (filename, "rt") as f:
     print("Number of Samples available for each type: ")
 
 # I then ask the program to count the information from the file, and using the headings ('Petal length' etc.) find the median, mean and max numbers of the information available
-# This is done below by using the 'describe' function.
+# This is done below by using the 'describe' function. I also add a line to gett he program to group the three species by their 'Type' aand use the .corr() function to find the pairwise
+# correlation of all the columns in the dataframe while also ignoring any non numeric data.
 
     print (iris['Type'].value_counts())
     print(' ')
     print (iris.describe())
+    print (iris.groupby("Type").corr())
+
    
 # We then start by organising the data within the file to start making sense of the data. This is done below by telling the program the location of the data we wish to seperate and analyse.
 # For example the iris_setosa will be found the in data file provided, under the heading of 'Type' - which the program will group information for those with the distinction 
@@ -52,6 +54,7 @@ sns.FacetGrid(iris,hue="Type",height=5).map(sns.histplot,"Petal Width").add_lege
 sns.FacetGrid(iris,hue="Type",height=5).map(sns.histplot,"Sepal Length").add_legend()
 sns.FacetGrid(iris,hue="Type",height=5).map(sns.histplot,"Sepal Width").add_legend()
 plt.show()
+plt.clf
 
 # I also wrote below a different output which looks to me more appealing that the simple hsitograms - I create a distplot instead of a histplot above. I prefer the look of the dist plot 
 # and the trend line makes the plot more engaging to read (in my opinion...)
@@ -61,6 +64,7 @@ sns.FacetGrid(iris,hue="Type",height=5).map(sns.distplot,"Petal Width").add_lege
 sns.FacetGrid(iris,hue="Type",height=5).map(sns.distplot,"Sepal Length").add_legend()
 sns.FacetGrid(iris,hue="Type",height=5).map(sns.distplot,"Sepal Width").add_legend()
 plt.show()
+plt.clf()
 
 # At this point I had to change the file name in the program from 'data' to 'iris' as it was confusing the program when it came to plotting the scatter plots (ie. could not have two itterations of 'data')
 # This is shown below and throughout the program above. I also noticed that in the previous itteration of the program I had mislabeled the 'Type' with capital Letters instead of lower case.
@@ -72,8 +76,10 @@ sns.scatterplot(data = iris_virginica, x= "Sepal Length", y = "Sepal Width", leg
 sns.scatterplot(data = iris_versicolor, x= "Sepal Length", y = "Sepal Width", legend = True)
 plt.legend(labels=["Iris Setosa", "Iris Virginica", "Iris Versicolor"])
 plt.suptitle("Iris Setosa, Virginica and Versicolour Sepal Length/Sepal Width")
+plt.show()
 plt.savefig('seanelliottSLSW.png', dpi = 500)
 plt.clf()
+
 
 # I was having alot of trouble with seaborn carrying the old information over into the newest plot - this was confusing the data I was trying to present. With a little researching I happened
 # upon the very useful 'plt.clf' which closes off one plot - making sure the information from the first plot doesnt bleed into the second. 
@@ -83,6 +89,7 @@ sns.scatterplot(data = iris_virginica, x= "Petal Length", y = "Petal Width", leg
 sns.scatterplot(data = iris_versicolor, x= "Petal Length", y = "Petal Width", legend = True)
 plt.legend(labels=["Iris Setosa", "Iris Virginica", "Iris Versicolor"])
 plt.suptitle("Iris Setosa, Virginica and Versicolour Petal Length/Petal Width")
+plt.show()
 plt.savefig('seanelliottPLPW.png', dpi = 500)
 plt.clf()
 
@@ -91,3 +98,22 @@ plt.clf()
 sns.set_style("darkgrid")
 sns.pairplot(iris,hue="Type",height=4)
 plt.show()
+
+# As an added bonus I have decided to add a boxplot to try something different. This is the same analysis as above - however it represents the data in a different way than the histograms/scatter plots.  
+
+sns.set(style="darkgrid", palette="deep")
+f, axes = plt.subplots(2, 2, sharey=False, figsize=(12, 8))
+f, axes = plt.subplots(2, 2, sharey=False, figsize=(12, 8))
+sns.boxplot(x="Type", y="Petal Length",hue = "Type",data=iris, ax=axes[0,0])
+sns.boxplot(x="Type", y="Sepal Length", data=iris, ax=axes[0,1])
+sns.boxplot(x="Type", y="Petal Width",data=iris, ax=axes[1,0])
+sns.boxplot(x="Type", y="Sepal Width", data=iris, ax=axes[1,1])
+plt.suptitle("Boxplot of the Petal and Sepal measurements")
+plt.show()
+
+# I also came accross a 'joint plot' on the seaborn website which I thought was a very interesting way of compairing the data. Below I ask the program to compare Petal Length and Sepal length across
+# the three species. I am particularly drawn to the representation of the data with the scatterplot in the middle with the trendlines on the exterior.
+
+sns.jointplot(data=iris, x = "Petal Length", y = "Sepal Length", hue = "Type")
+plt.show()
+ 
